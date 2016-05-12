f01b372119f7b5ae46f75c551a96f8863381fcd7
刘_Vicky <15332296@qq.com>
2016-05-12 12:33:21 +0800

    Update rlogin.php

--- rlogin.php
+++ rlogin.php
@@ -3,7 +3,7 @@ if(isset($_SESSION['open'])){
     echo '<script language="javascript">location.href="http://www.thinkout-art.com/reading/submit.php";</script>';
     exit;}
 
-$APPID='wx77be4299b302c1c1';
+$APPID='';//未给出
 $REDIRECT_URI='http://www.thinkout-art.com/reading/ruser.php';
 $scope='snsapi_userinfo';
 //$scope='snsapi_userinfo';//需要授权
